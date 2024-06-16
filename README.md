16412674656d3bb9dafdeaf77ebdba9a5a6dd506
Yuukyuu-Kuusou <yuukyuu.kuusou@gmail.com>
2024-06-16 17:22:30 -0300

    Simple algorithim

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Sort by average
- Just a random algorythim i tought about while studying about sorting algorythims
+  I decided to learn programming again and decided to take a look at algorithms, and while exploring sorting algorithms, I had an idea to create one that sorts based on the average value of the numbers in a list. This code is my attempt at it.
---
+++ sort.py
@@ -0,0 +1,45 @@
+import numpy as np
+def generate_unordered_list(list_edge):
+    unordered_list = list(range(-list_edge, list_edge + 1))
+    np.random.shuffle(unordered_list)
+    
+    return unordered_list
+
+def generate_uneven_list(size):
+    uneven_list = []
+    
+    for i in range(size):
+        num = np.random.randint(1, 1001)
+        uneven_list.append(num)
+    
+    return uneven_list
+
+def avg_sort(numbers_list, old_list = None):
+    if len(numbers_list) <= 1 or old_list == numbers_list:
+        return numbers_list
+    avg = sum(numbers_list) / len(numbers_list)
+    halfs = [[],[]]
+    for i in numbers_list:
+        if i < avg:
+            halfs[0].append(i)
+            continue
+        halfs[1].append(i)
+        continue
+    halfs[0] = avg_sort(halfs[0], numbers_list)
+    halfs[1] = avg_sort(halfs[1], numbers_list)
+    return halfs[0] + halfs[1]
+
+
+
+list_edge = 256
+numbers = generate_unordered_list(list_edge)
+
+print("Unordered List:",numbers)
+
+print("Ordered List:",avg_sort(numbers))
+
+numbers = generate_uneven_list(256)
+
+print("Unordered Uneven List:",numbers)
+
+print("Ordered Uneven List:",avg_sort(numbers))
